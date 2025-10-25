9b451264e5cd5f41a8de1422d03f6f125d7fdf99
felipebrunet <felipe.brunet@gmail.com>
2025-10-25 18:36:04 -0300

    version stuff for play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cl.icripto.xmrpos"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
